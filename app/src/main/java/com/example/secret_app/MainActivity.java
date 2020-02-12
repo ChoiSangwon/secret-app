6bdcffc3e7e83bfde69c50f117f36d56ef4065cc
ChoiSangwon <fdsa200@naver.com>
2020-02-12 13:39:32 +0900

    계산기 소스 추가 (사칙연산 가능)

--- app/src/main/java/com/example/secret_app/MainActivity.java
+++ app/src/main/java/com/example/secret_app/MainActivity.java
@@ -16,86 +16,179 @@ import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
-    Stack<Integer> stack = new Stack<>();
+    //Stack<Character> stack = new Stack<>();
+    Stack<Character> sign = new Stack<>();
     String aa=new String();
     TextView textView;
-    int result=66;
-
+    //int result=66;
+    double result=0;
+    int count=0;
+    int gg=4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.textView);
 
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                aa+=" = ";
+                textView.setText(aa);
+                String[] array=aa.split(" ");
 
+                //Stack<Integer> operate = new Stack<Integer>();
+                Stack<String> stack = new Stack<String>();
+                for(int i=0;i<array.length;i++){
+                    System.out.println(array[i]);
+                }
+                for(int i=0; i<array.length; i++) stack.add(array[i]);
+
+                for(int i=1; i<stack.size(); i=i+2){
+                    switch (stack.get(i)) {
+                        case "*": result = Double.valueOf(stack.get(i-1)) * Double.valueOf(stack.get(i+1));
+                            count++;
+                            stack.add(i-1, String.valueOf(result));
+                            stack.remove(i); stack.remove(i); stack.remove(i); i=i-2;break;
+                        case "/": result = Double.valueOf(stack.get(i-1)) / Double.valueOf(stack.get(i+1));
+                            count++;
+                            stack.add(i-1, String.valueOf(result));
+                            stack.remove(i); stack.remove(i); stack.remove(i); i=i-2;break;
+                        default: break;
+                    }
+                }
+                for(int i=1; i<stack.size(); i=i+2){
+                    if(i==1){
+                        result=0;
+                        result = result+Double.valueOf(stack.get(i-1));
+                    }switch(stack.get(i)){
+                        case "+": count++;System.out.print("|"+count+". "+result+" + "+Double.valueOf(stack.get(i+1))+" = ");
+                            result = result+Double.valueOf(stack.get(i+1)); break;
+                        case "-": count++;System.out.print("|"+count+". "+result+" - "+Double.valueOf(stack.get(i+1))+" = ");
+                            result = result-Double.valueOf(stack.get(i+1)); break;
+                    }
+                }
+
+                textView.setText(Double.toString(result));
+                aa="";
+            }
+        });
+
+
+
+    }
+    public void cla(View v){
+        int a=5;
+        if(v.getId()==R.id.button)
+            textView.setText(a);
     }
     public void clickSign(View v){
         switch(v.getId()){
             case R.id.plus:
-                //stack.push(-1);
-                aa+='+';
+                //stack.push('+');
+                aa+=" + ";
+                textView.setText(aa);
                 break;
             case R.id.sub:
-                aa+='-';
+                aa+=" - ";
+                textView.setText(aa);
                 break;
             case R.id.mul:
-                aa+='*';
+                aa+=" * ";
+                textView.setText(aa);
                 break;
             case R.id.divide:
-                aa+='/';
+                aa+=" / ";
+                textView.setText(aa);
                 break;
+                default:
+                    textView.setText(aa);
+                    break;
         }
-        if(R.id.equl==v.getId()){
-            if(result == 66){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"));
-                startActivity(intent);
+        /*if(R.id.equl==v.getId()){
+            //String[] array=aa.split(" ");
+
+            int result=0;
+            int count=0;
+            //Stack<Integer> operate = new Stack<Integer>();
+            //Stack<String> stack = new Stack<String>();
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+            for(int i=0; i<array.length; i++) stack.add(array[i]);
+
+            for(int i=1; i<stack.size(); i=i+2){
+                switch (stack.get(i)) {
+                    case "*": result = Integer.valueOf(stack.get(i-1)) * Integer.valueOf(stack.get(i+1));
+                        count++;
+                        stack.add(i-1, String.valueOf(result));
+                        stack.remove(i); stack.remove(i); stack.remove(i); i=i-2;break;
+                    case "/": result = Integer.valueOf(stack.get(i-1)) / Integer.valueOf(stack.get(i+1));
+                        count++;
+                        stack.add(i-1, String.valueOf(result));
+                        stack.remove(i); stack.remove(i); stack.remove(i); i=i-2;break;
+                    default: break;
+                }
             }
+            for(int i=1; i<stack.size(); i=i+2){
+                if(i==1){
+                    result=0;
+                    result = result+Integer.valueOf(stack.get(i-1));
+                }switch(stack.get(i)){
+                    case "+": count++;System.out.print("|"+count+". "+result+" + "+Integer.valueOf(stack.get(i+1))+" = ");
+                        result = result+Integer.valueOf(stack.get(i+1)); break;
+                    case "-": count++;System.out.print("|"+count+". "+result+" - "+Integer.valueOf(stack.get(i+1))+" = ");
+                        result = result-Integer.valueOf(stack.get(i+1)); break;
+                }
+            }
+            textView.setText(result);
         }
         else
-            textView.setText(aa);
+            textView.setText(aa);*/
     }
 
     public void clickNumber(View v){
         switch (v.getId()){
             case R.id.number0:
-                stack.push(0);
-                aa+='0';
+                //stack.push('0');
+                aa+="0";
                 break;
             case R.id.number1:
-                stack.push(1);
-                aa+='1';
+                //stack.push('1');
+                aa+="1";
                 break;
             case R.id.number2:
-                stack.push(2);
-                aa+='2';
+                //stack.push('2');
+                aa+="2";
                 break;
             case R.id.number3:
-                stack.push(3);
-                aa+='3';
+                //stack.push('3');
+                aa+="3";
                 break;
             case R.id.number4:
-                stack.push(4);
-                aa+='4';
+                //stack.push('4');
+                aa+="4";
                 break;
             case R.id.number5:
-                stack.push(5);
-                aa+='5';
+                //stack.push('5');
+                aa+="5";
                 break;
             case R.id.number6:
-                stack.push(6);
-                aa+='6';
+                //stack.push('6');
+                aa+="6";
                 break;
             case R.id.number7:
-                stack.push(7);
-                aa+='7';
+                //stack.push('7');
+                aa+="7";
                 break;
             case R.id.number8:
-                stack.push(8);
-                aa+='8';
+                //stack.push('8');
+                aa+="8";
                 break;
             case R.id.number9:
-                stack.push(9);
-                aa+='9';
+                //stack.push('9');
+                aa+="9";
                 break;
         }
         textView.setText(aa);
